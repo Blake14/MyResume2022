d1fb9293d7633d8708efc0f7478330ceb2258084
Blake14 <blakemcpherson2@gmail.com>
2022-07-21 12:44:10 -0500

    style: about page material

--- client/src/AboutMe.js
+++ client/src/AboutMe.js
@@ -2,10 +2,41 @@
 
 export const AboutMe = () => {
   return <div style={{
-    margin: 100
+    margin: 50
   }}>
     <p style={{
       fontSize: 24
+    }}>Links</p>
+    <a href='https://www.linkedin.com/in/blake14/'>https://www.linkedin.com/in/blake14/ </a> <br />
+    <a href='https://www.linkedin.com/in/blake14/'>https://www.github.com/blake14/ </a>
+    <br /><hr />
+    <p style={{
+      fontSize: 24
     }}>Education</p>
+    <div style={{
+      marginTop: 50,
+      width: 600
+    }}>
+      <p><strong>College of Engineering, <span style={{
+        color: '#CC0033'
+      }}>University of Oklahoma</span></strong> - Norman, Oklahoma</p>
+      <p>Belonged to an Engineering Team, which I collaborated with various types of engineers to build a race car to compete in races around the world.</p>
+    </div>
+    <div style={{
+      marginTop: 50
+    }}>
+      <p><strong>Associate of Applied Science, <span style={{
+        color: '#CC0033'
+      }}>Tulsa Community College</span></strong> - Tulsa, Oklahoma</p>
+    </div>
+    <div style={{
+      marginTop: 50
+    }}>
+      <p><strong>Bachelor's in Accountancy, Minor in Management, <span style={{
+        color: '#CC0033'
+      }}>Northeastern State University</span></strong> - Broken Arrow, Oklahoma</p>
+      <p>Graduated in 2019.</p>
+      <p>McGraw-Hill's Glo-Bus - Developing Winning Competitive Strategies - 1st Place Winner </p>
+    </div>
   </div>
 }
