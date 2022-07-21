ba75a711b1a7354c758c26acee251a3172e3baba
Blake14 <blakemcpherson2@gmail.com>
2022-07-21 12:14:36 -0500

    data: import portfolio data

---
+++ client/src/PortfolioItems.js
@@ -0,0 +1,63 @@
+import Project1Cover from './assets/resume-cover-photo.png'
+import Project2Cover from './assets/factory-blender-cover.png'
+import Project3Cover from './assets/MESWikiImage.png'
+import Project4Cover from './assets/WoodenFloor.png'
+import Project5Cover from './assets/Bedroom-Blender.png'
+import Project6Cover from './assets/unleashed-img.png'
+
+export const PortfolioItems = [
+  {
+    id: 1,
+    name: 'Portfolio React Webpage',
+    skills: ['Javascript', 'CSS', 'ReactJS', 'HTML', 'Web Design'],
+    images: [Project1Cover],
+    category: 'Web-Development',
+    summary: '',
+    review: ''
+  },
+  {
+    id: 2,
+    name: 'MES Wiki',
+    skills: ['Javascript', 'CSS', 'ReactJS', 'HTML', 'Web Design'],
+    images: [Project3Cover],
+    category: 'Web-Development',
+    summary: '',
+    review: ''
+  },
+  {
+    id: 3,
+    name: 'Factory 3D Model',
+    skills: ['3D', 'Animation', 'Design', 'Game Development'],
+    images: [Project2Cover],
+    category: 'Blender',
+    summary: '',
+    review: ''
+  },
+  {
+    id: 4,
+    name: 'Empty Room 3D Model',
+    skills: ['3D', 'Design'],
+    images: [Project4Cover],
+    category: 'Blender',
+    summary: '',
+    review: ''
+  },
+  {
+    id: 5,
+    name: 'Bedroom 3D Model',
+    skills: ['3D', 'Design'],
+    images: [Project5Cover],
+    category: 'Blender',
+    summary: '',
+    review: ''
+  },
+  {
+    id: 6,
+    name: 'Dog Treat Online Store',
+    skills: ['Javascript', 'CSS', 'ReactJS', 'HTML', 'Web Design'],
+    images: [Project6Cover],
+    category: 'Web-Development',
+    summary: '',
+    review: ''
+  }
+]
